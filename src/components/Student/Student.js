8d82b2049fe2c8bdad44b2c3ce224b43dc240816
Parmesanio <sean_parmar@yahoo.com>
2018-08-23 21:10:24 -0700

    Added go back buttons and loading spinner

--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -1,30 +1,43 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
+import './students.css';
 
 export default class Student extends Component {
   constructor() {
     super()
     this.state = {
-      studentInfo: {}
+      studentInfo: {},
+      isLoading: true
     }
+    this.goBack = this.goBack.bind(this);
   }
 componentDidMount() {
   axios.get(`http://localhost:3005/students/${this.props.match.params.id}`)
     .then(res => {
       this.setState({
-        studentInfo: res.data
+        studentInfo: res.data,
+        isLoading: false
       })
     })
 }
+goBack(){
+  this.props.history.goBack();
+}
   render() {    
-    let { studentInfo } = this.state;
+    let { studentInfo, isLoading } = this.state;
     let { first_name, last_name, grade, email} = studentInfo;
     return (
       <div className="box">
         <h1>Student</h1>
+        {isLoading ? 
+      <span className='loader'>Loading...</span>  
+       :
+       null }
         <h1>{first_name} {last_name}</h1>
         <h3>Grade: {grade}</h3>
         <h3>Emai: {email}</h3>
+        <button onClick={this.goBack}>Go Back</button>
       </div>
     )
   }
